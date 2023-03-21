cc1e66b66cd6ae4032a777bcc9ea34f74faae183
AceMouse <humle000@gmail.com>
2023-03-21 21:26:08 +0100

    multithread WeightedBoardAI

--- OthelloProject/WeightedBoardAI.java
+++ OthelloProject/WeightedBoardAI.java
@@ -1,14 +1,6 @@
 import java.lang.Math;
+import java.util.stream.IntStream;
 public class WeightedBoardAI implements IOthelloAI{
-    private class Pair<A, B>{
-        public A a;
-        public B b;
-        public Pair(A a, B b){
-            this.a=a;
-            this.b=b;
-        }
-    }
-
     private GameState clone(GameState s){
         return new GameState(s.getBoard(), s.getPlayerInTurn());
     }
@@ -31,7 +23,7 @@ public class WeightedBoardAI implements IOthelloAI{
     }
 
     private Position ABSearch(GameState s){
-        return maxValue(s, Integer.MIN_VALUE, Integer.MAX_VALUE, 6, s.getPlayerInTurn()).b;
+        return firstMaxValue(s, Integer.MIN_VALUE, Integer.MAX_VALUE, 6, s.getPlayerInTurn());
     }
 
     private int[][] generatePosValue(int size){
@@ -213,56 +205,72 @@ public class WeightedBoardAI implements IOthelloAI{
         return util;
     }
 
-    private Pair<Integer,Position> maxValue(GameState s, int alpha, int beta, int count, int me){
+    private Position firstMaxValue(GameState s, int alpha, int beta, int count, int me){
         boolean fin = s.isFinished();
         if (fin || count <= 0) 
-            return new Pair<>(utility(s,me, fin), null);
+            return null;
         int v = Integer.MIN_VALUE;
         Position move = null;
         var moves = s.legalMoves();
         if (moves.isEmpty())
             moves.add(new Position(-1, -1));
-        for(Position a : moves){
+        int[] vs = new int[moves.size()];
+        IntStream.range(0, moves.size()).parallel().forEach(i->{
+            Position a = moves.get(i);
             GameState sPrime = clone(s);
             sPrime.insertToken(a);
-            Pair<Integer, Position> min_choice = minValue(sPrime,alpha,beta, count-1, me);
-            int v2 = min_choice.a;
-            Position a2 = min_choice.b;
-            if (v2>v){
-               v = v2;
-               move = a;
-               alpha = Math.max(alpha, v);
+            vs[i] = minValue(sPrime,alpha,beta, count-1, me);
+        });
+        for(int i = 0; i < vs.length; i++){
+            if (vs[i]>v){
+                v = vs[i];
+                move = moves.get(i);
             }
-            if (v >= beta) 
-                return new Pair<Integer,Position>(v,move);
         }
-        return new Pair<Integer,Position>(v,move);
+        return move;
     }
-
-    private Pair<Integer,Position> minValue(GameState s, int alpha, int beta, int count, int me){
+    private int maxValue(GameState s, int alpha, int beta, int count, int me){
         boolean fin = s.isFinished();
         if (fin || count <= 0) 
-            return new Pair<>(utility(s,me, fin), null);
-        int v = Integer.MAX_VALUE;
-        Position move = null;
+            return utility(s,me, fin);
+        int v = Integer.MIN_VALUE;
         var moves = s.legalMoves();
         if (moves.isEmpty())
             moves.add(new Position(-1, -1));
         for(Position a : moves){
             GameState sPrime = clone(s);
             sPrime.insertToken(a);
-            Pair<Integer, Position> max_choice = maxValue(sPrime,alpha,beta, count-1, me);
-            int v2 = max_choice.a;
-            Position a2 = max_choice.b;
+            int v2 = minValue(sPrime,alpha,beta, count-1, me);
+            if (v2>v){
+               v = v2;
+               alpha = Math.max(alpha, v);
+            }
+            if (v >= beta) 
+                return v;
+        }
+        return v;
+    }
+
+    private int minValue(GameState s, int alpha, int beta, int count, int me){
+        boolean fin = s.isFinished();
+        if (fin || count <= 0) 
+            return utility(s,me, fin);
+        int v = Integer.MAX_VALUE;
+        var moves = s.legalMoves();
+        if (moves.isEmpty())
+            moves.add(new Position(-1, -1));
+        for(Position a : moves){
+            GameState sPrime = clone(s);
+            sPrime.insertToken(a);
+            int v2 = maxValue(sPrime,alpha,beta, count-1, me);
             if (v2<v){
                v = v2;
-               move = a;
                alpha = Math.min(alpha, v);
             }
             if (v <= alpha) 
-                return new Pair<Integer,Position>(v,move);
+                return v;
         }
-        return new Pair<Integer,Position>(v,move);
+        return v;
     }
 
 }
